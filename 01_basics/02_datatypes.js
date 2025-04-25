662920035c7a211cd15a33a1ea922f8811a453d3
Ansh Verma <133218395+vermagAnsh@users.noreply.github.com>
2025-04-25 19:27:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+
+// number = 2 to power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => stanalone Value
+// undefined => 
+// Symbol => unique 
+// object
+
+// the typeof null is "object"
+console.log(typeof null);
+
+// the type of undefined is "undefined"
+console.log(typeof undefined);
